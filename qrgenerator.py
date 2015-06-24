02577b084a8771b9dd9cadc1f09027653a39072b
Anderson Foscarini <anderson@foscarini.biz>
2015-06-24 01:54:54 -0300

    adicionado halftone

--- qrgenerator.py
+++ qrgenerator.py
@@ -19,14 +19,35 @@ def create_qrcode(text, image_path):
     #print image_path
     imagef=Image.open(image_path)
     imagef=imagef.convert('L')
-    imagef.thumbnail(im.size)
-    newsize0 = (im.size[0] - imagef.size[0])
-    newsize1 = (im.size[1] - imagef.size[1])
-    newsize = (newsize0+18, newsize1+18)
-    thum = ImageOps.expand(imagef,border=newsize, fill='white')
-    thum = ImageOps.fit(thum,im.size)
+    if im.size < imagef.size:
+        imagef.thumbnail(im.size)
+        newsize0 = (im.size[0] - imagef.size[0])
+        newsize1 = (im.size[1] - imagef.size[1])
+        newsize = (newsize0+18, newsize1+18)
+        thum = ImageOps.expand(imagef,border=newsize, fill='white')
+        thum = ImageOps.fit(thum,im.size)
+    else:
+        thum = ImageOps.fit(imagef,im.size)
 
     #thum.save('thum.png')
+    for i in range(0, im.size[0]-1):
+        for j in range(0, im.size[1]-1):
+            cc = thum.getpixel((i,j)) #// current color
+            if cc < 128:
+                rc=0
+            else:
+                rc=255      #real (rounded) color
+            err = cc-rc              # error amount
+            thum.putpixel((i,j), rc)#   saving real color
+            if j+1 < im.size[1]:
+                thum.putpixel((i,j+1), thum.getpixel((i,j+1))+(err*7)/16)# if right neighbour exists
+            if i+1 == im.size[0] :
+                pass   #// if we are in the last line
+            if j > 0 :
+                thum.putpixel((i+1,j-1), thum.getpixel((i+1,j-1))+(err*3)/16) #bottom left neighbour
+                thum.putpixel((i+1,j), thum.getpixel((i+1,j))+(err*5)/16)  #bottom neighbour
+            if j+1 < im.size[1] :
+                thum.putpixel((i+1,j+1), thum.getpixel((i+1,j+1))+(err*1)/16) #bottom right neighbour
 
     for i in range(0,81):
         for j in range(0,81):
